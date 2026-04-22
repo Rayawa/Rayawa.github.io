8c6df453a46b097eb1dcc9be907b79a20fa768b0
Rayawa <171078364@qq.com>
2026-04-23 00:48:59 +0800

    docs(projects): 更新生物学项目论文标题和摘要
    更新了中文、英文和法文版本的生物学项目页面，交换了两篇论文的标题和摘要内容。将关于朊病毒蛋白的研究与关于蛋白质作为遗传物质的猜想进行了位置调换，以更好地反映项目进展状态。

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -23,11 +23,11 @@
     var API_BASE = 'http://ddns.shenjack.top:10003';
 
    var mockData = {
-    totalViews: 1694929,
-    totalRequests: 100936798, harmonyTotalRequests: 1059354, todayRequests: 802067,
+    totalViews: 1696245,
+    totalRequests: 100988681, harmonyTotalRequests: 1059908, todayRequests: 802067,
 };
 
-var updateTime = '2026-04-22T15:28:51Z';
+var updateTime = '2026-04-22T16:33:51Z';
 
 function updateTimestamp() {
     var noteEl = document.querySelector('.metrics-note');
